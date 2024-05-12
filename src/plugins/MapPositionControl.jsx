3cfc2cafa42e833dc4e76d43056b0d86fad88556
alqassas9 <aalqassas@lucidya.com>
2024-05-13 02:54:31 +0300

    create map plugin to show the mouse position

---
+++ src/plugins/MapPositionControl.jsx
@@ -0,0 +1,30 @@
+import { useEffect } from "react";
+import { MousePosition } from "ol/control";
+import { useSelector } from "react-redux";
+import { selectMapObject } from "../redux/MapSlice";
+
+export const MapPositionControl = () => {
+  const mapObject = useSelector(selectMapObject);
+
+  useEffect(() => {
+    if (!mapObject) return;
+    const mousePositionControl = new MousePosition({
+      coordinateFormat: (coordinates) =>
+        `Latitude: ${coordinates[1].toFixed(
+          6
+        )}, Longitude: ${coordinates[0].toFixed(6)}`,
+      projection: "EPSG:4326",
+      className: "custom-mouse-position",
+
+      undefinedHTML: "&nbsp;",
+    });
+
+    mapObject.controls.push(mousePositionControl);
+
+    return () => mapObject.controls.remove(mousePositionControl);
+  }, [mapObject]);
+
+  return null;
+};
+
+export default MapPositionControl;
