149ced712fbe9a95769e005ff266d0a85792becc
alqassas9 <aalqassas@lucidya.com>
2024-05-17 03:40:10 +0300

    creat a line example to conect

--- src/App.jsx
+++ src/App.jsx
@@ -17,12 +17,17 @@ import "./App.css";
 import MapComponent from "./components/map";
 import { MapPositionControl } from "./plugins/MapPositionControl";
 import ZoomControl from "./plugins/zoomControl";
-
+import * as ol from "ol";
+import LineString from "ol/geom/LineString.js";
+import Vector from "ol/source/Vector";
+import VectorLayer from "ol/layer/Vector";
+import Style from "ol/style/Style";
+import Stroke from "ol/style/Stroke";
 function App() {
   const zoomLevel = useSelector(selectZoomLevel);
   const centerPoint = useSelector(selectCenterPoint);
   const mapObject = useSelector(selectMapObject);
-  console.log(centerPoint, zoomLevel);
+
   const dispatch = useDispatch();
 
   const mapRef = useRef(null);
@@ -53,7 +58,45 @@ function App() {
       document.getElementById("zoomlevel").innerHTML = zoomInfo;
     });
   }
+  useEffect(() => {
+    if (mapObject) {
+      const routeCoordinates = [
+        [31.2497, 30.0626], // Cairo (Point A)
+        [29.9553, 31.2156], // Alexandria (Point B)
+      ];
 
+      // Create a GeoJSON object representing the route
+      const routeGeoJSON = {
+        type: "Feature",
+        geometry: {
+          type: "LineString",
+          coordinates: routeCoordinates,
+        },
+      };
+
+      // Now you can use the routeGeoJSON directly in your OpenLayers code
+      const routeFeature = new ol.Feature({
+        geometry: new LineString(routeGeoJSON.geometry.coordinates).transform(
+          "EPSG:4326",
+          "EPSG:3857"
+        ),
+      });
+
+      const vectorLayer = new VectorLayer({
+        source: new Vector({
+          features: [routeFeature],
+        }),
+        style: new Style({
+          stroke: new Stroke({
+            color: "green", // Red line color
+            width: 5,
+          }),
+        }),
+      });
+
+      mapObject.addLayer(vectorLayer);
+    }
+  }, [mapObject]);
   return (
     <>
       <MapComponent
