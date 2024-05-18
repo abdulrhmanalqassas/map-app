30af5cedf34c2d199f88007634f973f2ea3fdbe1
alqassas9 <aalqassas@lucidya.com>
2024-05-18 05:45:50 +0300

    create Draw on map  Control plugin

---
+++ src/plugins/mapDrawControl.jsx
@@ -0,0 +1,96 @@
+import { useEffect } from "react";
+import { useDispatch, useSelector } from "react-redux";
+import { selectMapObject } from "../redux/MapSlice";
+import Draw from "ol/interaction/Draw.js";
+import Vector from "ol/source/Vector";
+import VectorLayer from "ol/layer/Vector";
+export const MapDrawControl = () => {
+  const mapObject = useSelector(selectMapObject);
+  const source = new Vector({ wrapX: false });
+  const vector = new VectorLayer({
+    source: source,
+  });
+
+  useEffect(() => {
+    if (!mapObject) return;
+    mapObject.addLayer(vector);
+    const styles = {
+      Point: {
+        "circle-radius": 5,
+        "circle-fill-color": "red",
+      },
+      LineString: {
+        "circle-radius": 5,
+        "circle-fill-color": "red",
+        "stroke-color": "yellow",
+        "stroke-width": 2,
+      },
+      Polygon: {
+        "circle-radius": 5,
+        "circle-fill-color": "red",
+        "stroke-color": "yellow",
+        "stroke-width": 2,
+        "fill-color": "blue",
+      },
+      Circle: {
+        "circle-radius": 5,
+        "circle-fill-color": "red",
+        "stroke-color": "blue",
+        "stroke-width": 2,
+        "fill-color": "yellow",
+      },
+    };
+    const typeSelect = document.getElementById("type");
+    let draw; // global so we can remove it later
+    function addInteraction() {
+      const value = typeSelect.value;
+      if (value !== "None") {
+        draw = new Draw({
+          source: source,
+          type: typeSelect.value,
+          style: styles[value],
+        });
+        mapObject.addInteraction(draw);
+      }
+    }
+    /**
+     * Handle change event.
+     */
+    typeSelect.onchange = function () {
+      mapObject.removeInteraction(draw);
+      addInteraction();
+    };
+    addInteraction();
+  }, [mapObject]);
+
+  let clearMap = () => {
+    // mapObject
+    //   .getLayers()
+    //   .getArray()
+    //   .slice()
+    //   .forEach((layer) => mapObject.removeLayer(layer));
+    source.clear();
+    vector.getSource().refresh();
+  };
+  return (
+    <>
+      <div className="row">
+        <div className="col-auto">
+          <span className="input-group">
+            <label className="input-group-text" htmlFor="type">
+              Geometry type:
+            </label>
+            <select className="form-select" id="type">
+              <option value="None">None</option>
+              <option value="Point">Point</option>
+              <option value="LineString">LineString</option>
+              <option value="Polygon">Polygon</option>
+              <option value="Circle">Circle</option>
+            </select>
+          </span>
+          <button onClick={() => clearMap()}>clear map</button>
+        </div>
+      </div>
+    </>
+  );
+};
